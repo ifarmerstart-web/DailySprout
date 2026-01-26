f6cd77c9f34d14c18a5a26512665659b32dd2577
ifarmerstart-web <ifarmerstart@gmail.com>
2026-01-26 16:49:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-// 1. 아래 줄을 삭제하세요!
-// import tsconfigPaths from 'vite-tsconfig-paths'; 
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     base: '/DailySprout/', 
-    // 2. 아래 plugins 안의 tsconfigPaths()를 삭제하세요!
     plugins: [react() /*], 
     resolve: {
       alias: {
