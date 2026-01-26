b29d252a6ce7df206011fd7f38783e2475fedd09
ifarmerstart-web <ifarmerstart@gmail.com>
2026-01-26 16:11:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url'; // 추가
 import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths'
- 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
- 
-  return {
-    base: '/DailySprout/',
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
-    },
-  };
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // 추가
+
+export default defineConfig({
+  base: '/DailySprout/', 
+  plugins: [react()],
+  resolve: {
+    alias: [
+      // 이 방식으로 설정하면 조금 더 유연하게 경로를 잡습니다.
+      { find: '@', replacement: path.resolve(__dirname, 'src') }
+    ]
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 });
