4d462f81dbad65b27130071c0e98ddef2362e083
ifarmerstart-web <ifarmerstart@gmail.com>
2026-01-26 16:03:51 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths'
  
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
  
   return {
     base: '/DailySprout/',
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
