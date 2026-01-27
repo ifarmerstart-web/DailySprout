e7232354708f8fd7338e1e7e81d567aed66587d1
ifarmerstart-web <ifarmerstart@gmail.com>
2026-01-27 13:07:24 +0900

    Update GardenTimeline.tsx

--- src/components/GardenTimeline.tsx
+++ src/components/GardenTimeline.tsx
@@ -278,7 +278,7 @@ const GardenTimeline: React.FC<GardenTimelineProps> = ({
                                   <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
                                 </svg>
                               </button>
-                              <h3 className="font-black text-slate-800 text-5xl flex items-center gap-1.5 truncate">//작물이름
+                              <h3 className="font-black text-slate-800 text-2xl flex items-center gap-1.5 truncate">
                                 {crop.name}
                                 {isTooEarly && !item.isHarvested && <span className="text-amber-500 text-xs">⚠️</span>}
                               </h3>
